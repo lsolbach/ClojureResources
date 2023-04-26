2bed346df8746e2d0b87a069a6be6f1c92b07747
Ludger Solbach <ludger.solbach@msg.group>
2023-04-26 17:44:52 +0200

    added csv and json libs

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject ClojureResources "latest"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.csv "1.0.1"]
+                 [org.clojure/data.json "2.4.0"]]
   )
