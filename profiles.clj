9ecf72ad07878215a93cfcfe48017bbad6efe55d
Ludger Solbach <soulman@soulspace.org>
2024-05-22 07:56:34 +0200

    fixed profiles

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,12 @@
-{:profiles 
- :dev {:dependencies [[djblue/portal "0.55.1"]
-                      [criterium/criterium "0.4.6"]
-                      [com.clojure-goes-fast/clj-java-decompiler "0.3.4"]
+{:profiles
+ {:dev
+  {:dependencies [[djblue/portal "0.55.1"]
+                  [criterium/criterium "0.4.6"]
+                  [com.clojure-goes-fast/clj-java-decompiler "0.3.4"]
                       ; [expound/expound "0.9.0"]
-                      ]
-       :global-vars {*warn-on-reflection* true}}
- {:flowstorm
+                  ]
+   :global-vars {*warn-on-reflection* true}}
+  :flowstorm
   {:dependencies [[com.github.flow-storm/clojure "1.12.0-alpha9_1"]
                   [com.github.flow-storm/flow-storm-dbg "3.13.1"]]
    :exclusions [org.clojure/clojure]
@@ -14,5 +15,4 @@
               "-Dclojure.storm.instrumentAutoPrefixes=true"
              ;; with whatever editor you have
               "-Dflowstorm.jarEditorCommand=emacsclient -n +<<LINE>>:0 <<JAR>>/<<FILE>>"
-              "-Dflowstorm.fileEditorCommand=emacsclient -n +<<LINE>>:0 <<FILE>>"]}}
-}
+              "-Dflowstorm.fileEditorCommand=emacsclient -n +<<LINE>>:0 <<FILE>>"]}}}
