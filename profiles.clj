09338d4d6c3fc9bcf6b6f3709668571c2ed68600
Ludger Solbach <soulman@soulspace.org>
2024-05-22 10:05:03 +0200

    updated profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,18 +1,17 @@
-{:profiles
- {:dev
-  {:dependencies [[djblue/portal "0.55.1"]
-                  [criterium/criterium "0.4.6"]
-                  [com.clojure-goes-fast/clj-java-decompiler "0.3.4"]
+{:user
+ {:dependencies [[djblue/portal "0.55.1"]
+                 [criterium/criterium "0.4.6"]
+                 [com.clojure-goes-fast/clj-java-decompiler "0.3.4"]
                       ; [expound/expound "0.9.0"]
-                  ]
-   :global-vars {*warn-on-reflection* true}}
-  :flowstorm
-  {:dependencies [[com.github.flow-storm/clojure "1.12.0-alpha9_1"]
-                  [com.github.flow-storm/flow-storm-dbg "3.13.1"]]
-   :exclusions [org.clojure/clojure]
-   :jvm-opts ["-Dflowstorm.startRecording=false"
-              "-Dclojure.storm.instrumentEnable=true"
-              "-Dclojure.storm.instrumentAutoPrefixes=true"
+                 ]
+  :global-vars {*warn-on-reflection* true}}
+ :flowstorm
+ {:dependencies [[com.github.flow-storm/clojure "1.12.0-alpha9_1"]
+                 [com.github.flow-storm/flow-storm-dbg "3.13.1"]]
+  :exclusions [org.clojure/clojure]
+  :jvm-opts ["-Dflowstorm.startRecording=false"
+             "-Dclojure.storm.instrumentEnable=true"
+             "-Dclojure.storm.instrumentAutoPrefixes=true"
              ;; with whatever editor you have
-              "-Dflowstorm.jarEditorCommand=emacsclient -n +<<LINE>>:0 <<JAR>>/<<FILE>>"
-              "-Dflowstorm.fileEditorCommand=emacsclient -n +<<LINE>>:0 <<FILE>>"]}}}
+             "-Dflowstorm.jarEditorCommand=emacsclient -n +<<LINE>>:0 <<JAR>>/<<FILE>>"
+             "-Dflowstorm.fileEditorCommand=emacsclient -n +<<LINE>>:0 <<FILE>>"]}}
