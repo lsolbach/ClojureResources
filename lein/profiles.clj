1a5f295bf80e4c40409fa521fd35d1d9834e732a
Ludger Solbach <soulman@soulspace.org>
2024-09-20 12:46:48 +0200

    updated profiles.clj and deps.edn

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user
- {:dependencies [[djblue/portal "0.57.2"]
+ {:dependencies [[djblue/portal "0.57.3"]
                  [criterium/criterium "0.4.6"]
                  [com.clojure-goes-fast/clj-java-decompiler "0.3.6"]
                       ; [expound/expound "0.9.0"]
