3025157fdbb86a610dbc364132fc8348537a2320
Ludger Solbach <soulman@soulspace.org>
2025-09-21 21:58:45 +0200

    update nrepl and portal in lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:dependencies [[djblue/portal "0.60.2"]
+ {:dependencies [[nrepl/nrepl "1.4.0"]
+                 [djblue/portal "0.61.0"]
                  [criterium/criterium "0.4.6"]
                  [com.clojure-goes-fast/clj-java-decompiler "0.3.7"]
                       ; [expound/expound "0.9.0"]
